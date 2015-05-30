ca9f7892d1d5931aee451181805df429592d9f0d
Pierre Borgmann <drweissbrot@outlook.com>
2015-05-30 15:48:31 +0200

    Small fixes

--- cmd/php/stationinfo/vardump.php
+++ cmd/php/stationinfo/vardump.php
@@ -1,12 +1,3 @@
 <?php
-    require("currsong.php");
-    require("currevent.php");
-
-    if($autodj == true) {
-        echo "Now Playing: $currsong (DJ-Pony Lucy, $listener)
-Nächste Sendung: " . $nextevent["title"] . " (" . $nextevent["time_human"] . ")";
-    } else {
-        echo "Now Playing: $currsong (" . $currevent["title"] . ", $listener)
-Nächste Sendung: " . $nextevent["title"] . " (" . $nextevent["time_human"] . ")";
-    }
+    echo 'Bitte verwende song oder dj. vardump wird in Zukunft nicht mehr unterstützt.';
 ?>
