15a04730eea8e3b542d2f52d6c209eda3c7533e6
Pierre Borgmann <drweissbrot@outlook.com>
2015-09-10 23:22:20 +0200

    Add 'Hallöchen~' command
    For some reason I forgot to implement this in the first place...

--- filters/blah.sh
+++ filters/blah.sh
@@ -7,5 +7,10 @@ ts() {
     echo 'TeamSpeak 3-Server: 85.214.218.195:9988'
 }
 
+hello() {
+	bash cmd/query/hello
+}
+
 filter 'lucy_light, bot' bot
 filter 'lucy_light, ts' ts
+filter 'hallöchen~' hello
